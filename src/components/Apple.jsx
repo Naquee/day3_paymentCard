a9a6301313ee2166e0f126c4a5ff1a8d337f0d53
Naquee <naqueealam99@gmail.com>
2022-05-22 00:26:36 +0530

    payment

---
+++ src/components/Apple.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Apple.css";
+
+export const Apple = () => {
+  return (
+    <div>
+  
+    <div className='card2'>
+    <div className='card21'>
+    <p>28/10/2022</p>
+    <img
+src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/488px-Apple_logo_black.svg.png?20211218170823'></img>
+</div>
+    <div className='btn'> 
+    <button>Case Study</button>
+    </div>
+
+    <div>
+    <h1>Apple Gift</h1>
+    </div>
+    <div><h1>Payment</h1></div>
+    <div><h4>Destop-Mobile</h4></div>
+    
+
+    </div>
+    
+</div>
+
+    
+  )
+}
+export default Apple;
---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./card.css"
+
+
+export const Card = () => {
+  return (
+    <div>
+       
+        <div className='card1'>
+        <div className='card11'>
+        <p>28/10/2022</p>
+        <img
+ src='https://image.shutterstock.com/image-photo/montreal-canada-july-30-2017-600w-706518910.jpg'></img>
+  </div>
+        <div className='btn'> 
+        <button>Case Study</button>
+        </div>
+
+        <div>
+        <h1>amazone Gift</h1>
+        </div>
+        <div><h1>Pay</h1></div>
+        <div><h4>Destop-Mobile</h4></div>
+        
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Card;
